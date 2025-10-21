9d9b1b1fe668ff04ac6a3a47830fdbb81dbd2ea7
Bill Dushime (He/Him) <bdushime1@babson.edu>
2025-10-21 11:30:50 -0400

    feat: microsoft clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -35,6 +36,15 @@ export default function RootLayout({
   return (
     <html lang="el">
       <body className="antialiased" suppressHydrationWarning>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ttgmb3w4ly");
+          `}
+        </Script>
         {children}
       </body>
     </html>
